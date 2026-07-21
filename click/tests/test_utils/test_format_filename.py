6cc358eeb2fbbc33fb4594abb98cf680915546d8
a3643605788 <72342865+a3643605788@users.noreply.github.com>
2026-07-21 15:20:09 +0800

    fix: 移除click資料夾的git追蹤，改用.gitignore排除

--- click/tests/test_utils/test_format_filename.py
+++
@@ -1,9 +0,0 @@
-import click
-
-
-def test_filename_formatting():
-    assert click.format_filename(b"foo.txt") == "foo.txt"
-    assert click.format_filename(b"/x/foo.txt") == "/x/foo.txt"
-    assert click.format_filename("/x/foo.txt") == "/x/foo.txt"
-    assert click.format_filename("/x/foo.txt", shorten=True) == "foo.txt"
-    assert click.format_filename("/x/\ufffd.txt", shorten=True) == "�.txt"
